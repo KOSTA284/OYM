cb6165c51fcf280fe6041b0fda3b6d996d622c54
fly9702 <fiy9702@naver.com>
2024-07-16 10:55:29 +0900

    feat: add restaurants data

--- src/com/oym/vo/Chinese.java
+++ src/com/oym/vo/Chinese.java
@@ -1,9 +1,16 @@
 package com.oym.vo;
 
+import java.util.ArrayList;
+
 public class Chinese extends Restaurant{
 
 	public Chinese(String name, String tel, String location, int openTime, int closeTime) {
 		super(name, tel, location, openTime, closeTime);
+	}
+
+	public Chinese(String name, String tel, String location, int openTime, int closeTime, ArrayList<Review> reviews,
+			ArrayList<Menu> menus) {
+		super(name, tel, location, openTime, closeTime, reviews, menus);
 	}
 
 }
--- src/com/oym/vo/Japanese.java
+++ src/com/oym/vo/Japanese.java
@@ -1,9 +1,17 @@
 package com.oym.vo;
 
+import java.util.ArrayList;
+
 public class Japanese extends Restaurant{
 
 	public Japanese(String name, String tel, String location, int openTime, int closeTime) {
 		super(name, tel, location, openTime, closeTime);
+	}
+
+	public Japanese(String name, String tel, String location, int openTime, int closeTime, ArrayList<Review> reviews,
+			ArrayList<Menu> menus) {
+		super(name, tel, location, openTime, closeTime, reviews, menus);
+		// TODO Auto-generated constructor stub
 	}
 
 }
