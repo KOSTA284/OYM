d8325ce94eb9f8fb256d6e35220f9a458981a0ab
garubi04 <sleepone77@gmail.com>
2024-07-16 14:29:07 +0900

    add : 메뉴 추가

--- src/com/oym/vo/Menu.java
+++ src/com/oym/vo/Menu.java
@@ -1,5 +1,59 @@
 package com.oym.vo;
 
 public class Menu {
+	private String name;
+	private int price;
+	private String subCategory;
+	private String summary;
+	
+	
+	public Menu(String name, int price, String subCategory, String summary) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.subCategory = subCategory;
+		this.summary = summary;
+	}
 
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public String getSubCategory() {
+		return subCategory;
+	}
+
+
+	public void setSubCategory(String subCategory) {
+		this.subCategory = subCategory;
+	}
+
+
+	public String getSummary() {
+		return summary;
+	}
+
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	
+	
 }
